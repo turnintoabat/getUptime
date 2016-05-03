6853bded8b5d4c74325c63cb78dead510afc9190
Brendan Sturges <BrendanSturges@users.noreply.github.com>
2016-05-03 16:35:41 -0400

    rewrote job functionality

--- getUptime2.ps1
+++ getUptime2.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Function Get-FileName($initialDirectory){   
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
@@ -33,7 +32,8 @@ $scriptblock = {
 				$wmiresult = $wmi.Get()
 					$ErrorMessage = ''
 					$lastBootUpTime = ''
-					$uptime = ''					
+					$uptime = ''
+					$a = ''
 					
 					$Bootup = $wmiresult.LastBootUpTime
 					$LastBootUpTime = [System.Management.ManagementDateTimeconverter]::ToDateTime($Bootup)
@@ -43,13 +43,13 @@ $scriptblock = {
 					$h = $Uptime.Hours
 					$m = $uptime.Minutes
 					$s = $uptime.Seconds
-					$a = "$System Up for: {0} days, {1} hours, {2}.{3} minutes" -f $d,$h,$m,$s
+					$a = [string]$d + ":" + $h + ":" + $m + ":" + $s
 					
 					
 					$serverInfo = New-Object -TypeName PSObject -Property @{
 						Server = $system
 						LastBoot = $LastBootUpTime
-						Uptime = $Uptime
+						Uptime = $a
 						Details = $ErrorMessage
 					}
 			}
@@ -58,7 +58,7 @@ $scriptblock = {
 					$serverInfo = New-Object -TypeName PSObject -Property @{
 						Server = $system
 						LastBoot = $LastBootUpTime
-						Uptime = $Uptime
+						Uptime = $a
 						Details = $ErrorMessage
 					}
 				}
@@ -69,109 +69,7 @@ $scriptblock = {
 			$serverInfo = New-Object -TypeName PSObject -Property @{
 				Server = $system
 				LastBoot = $LastBootUpTime
-				Uptime = $Uptime
-				Details = $ErrorMessage
-			}
-
-	}
-	return $serverInfo
-}
-
-$serverList = Get-Content -Path (Get-FileName)
-$fileName = Save-File $fileName
-$i = 0
-$erroractionpreference = "SilentlyContinue"
-
-$jobs = @()
-foreach ($system in $serverList) {	
-	$jobs += Start-Job -ScriptBlock $scriptblock -ArgumentList $system
-	$i++
-	Write-Progress -activity "Starting Job $i of $($serverList.count)" -percentComplete ($i / $serverList.Count*100) 
-	}
-	
-#YO ADD THROTTLING TO MAKE THIS NOT SUCK
-$jobs | Wait-Job -timeout 120 -Job $jobs > $null
-$output = @()
-
-#!
-
-foreach($job in $jobs){
-	$output += $_ | Receive-Job $job | Select-Object Server,LastBoot,Uptime,Details
-	$output | Export-Csv $fileName -noTypeInformation -append
-}
-
-=======
-Function Get-FileName($initialDirectory){   
-	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-	$OpenFileDialog.initialDirectory = $initialDirectory
-	$OpenFileDialog.filter = "All files (*.*)| *.*"
-	$OpenFileDialog.ShowDialog() | Out-Null
-	$OpenFileDialog.filename
-}
-
-Function Save-File([string] $initialDirectory ) {
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = "All files (*.*)| *.*"
-    $OpenFileDialog.ShowDialog() |  Out-Null
-	
-	$nameWithExtension = "$($OpenFileDialog.filename).csv"
-	return $nameWithExtension
-}
-
-$scriptblock = {
-	param($system)
-	$rtn = Test-Connection -ComputerName $system -Count 1 -Quiet
-	if($rtn) {
-		$NameSpace = "Root\CIMV2"
-		$wmi = [WMISearcher]""
-		$wmi.options.timeout = '0:0:5'
-		$query = 'Select * from Win32_OperatingSystem'
-		$wmi.scope.path = "\\$system\$NameSpace"
-		$wmi.query = $query
-			Try{
-				$wmiresult = $wmi.Get()
-					$ErrorMessage = ''
-					$lastBootUpTime = ''
-					$uptime = ''					
-					
-					$Bootup = $wmiresult.LastBootUpTime
-					$LastBootUpTime = [System.Management.ManagementDateTimeconverter]::ToDateTime($Bootup)
-					$now = Get-Date
-					$Uptime = $now - $lastBootUpTime
-					$d = $Uptime.Days
-					$h = $Uptime.Hours
-					$m = $uptime.Minutes
-					$s = $uptime.Seconds
-					$a = "$System Up for: {0} days, {1} hours, {2}.{3} minutes" -f $d,$h,$m,$s
-					
-					
-					$serverInfo = New-Object -TypeName PSObject -Property @{
-						Server = $system
-						LastBoot = $LastBootUpTime
-						Uptime = $Uptime
-						Details = $ErrorMessage
-					}
-			}
-				Catch {
-					$ErrorMessage = $_.Exception.Message
-					$serverInfo = New-Object -TypeName PSObject -Property @{
-						Server = $system
-						LastBoot = $LastBootUpTime
-						Uptime = $Uptime
-						Details = $ErrorMessage
-					}
-				}
-	}	
-	
-	else{
-		$ErrorMessage = 'Server is Offline'
-			$serverInfo = New-Object -TypeName PSObject -Property @{
-				Server = $system
-				LastBoot = $LastBootUpTime
-				Uptime = $Uptime
+				Uptime = $a
 				Details = $ErrorMessage
 			}
 
@@ -190,25 +88,38 @@ foreach ($system in $serverList) {
 	$i++
 	Write-Progress -id 1 -activity "Starting Job $i of $($serverList.count)" -percentComplete ($i / $serverList.Count*100) 
 	}
-	
-#YO ADD THROTTLING TO MAKE THIS NOT SUCK
 
-#Wait-Job -timeout 120 -Job $jobs > $null
 $output = @()
+$output = get-job
+
+$finalObject = @()
+
 $j = 0
-foreach($job in $jobs){
-	$output = Get-Job | Wait-Job -timeout 30 | receive-job
-	$output | Select Server,Lastboot,Uptime,Details | Export-Csv $fileName -noTypeInformation -append
-	Write-Progress -id 2 -parentid 1 -activity "Starting Job $j of $($serverList.count)" -percentComplete ($j / $serverList.Count*100)
+
+foreach($job in $output){
+	
+
+	$holder = Receive-Job -id $job.id
+
+	while (@(Get-Job -State Running)) {
+		$j++
+		Write-Progress -id 2 -activity "Finishing Job $j of $($serverList.count)" -percentComplete ($j / $serverList.Count*100) 	
+		$now = Get-Date
+			foreach ($job in @(Get-Job -State Running)) {
+				if ($now - (Get-Job -Id $job.id).PSBeginTime -gt [TimeSpan]::FromSeconds(30)) {
+					Stop-Job $job
+				}
+			}
+		Start-Sleep -sec 2
+	}
+
+	if(($job.state = "Failed") -or ($job.state = "Stopped")){
+		$holder.Details = "WMI Timeout"
+	}
+	
+	$finalObject = $holder | Select Server,Lastboot,Uptime,Details | Export-Csv $fileName -noTypeInformation -append
 }
 
+get-job | Remove-Job
 
 
-<#
-$jobs | % {
-	$output += $_ | Receive-Job | Select-Object Server,LastBoot,Uptime,Details
-	$output | Export-Csv $fileName -noTypeInformation -append
-}
-#>
-
->>>>>>> fc180105da5839a86d8ebac291654fe57dd61f0e
